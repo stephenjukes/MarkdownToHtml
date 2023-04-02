25376d26bae9a14088614d939291b2c1907a848f
Stephen Jukes <contact@stephenjukes.com>
2023-04-02 23:23:24 +0100

    Added default markdown from https://markdown-it.github.io/

--- markdownToHtml.js
+++ markdownToHtml.js
@@ -322,57 +322,197 @@ function run() {
 const runButton = document.getElementById('run-button');
 runButton.addEventListener('click', run);
 
-
-// markdownSection.value = `
-// # Joe Bloggs
-
-// ![professional photo](https://www.neilsonreeves.co.uk/wp-content/uploads/corporate-headshot-plain-neutral-background.jpg)
-
-// ## Introduction
-// ---
-// Lorem _ipsum_ dolor sit amet, \`consectetur adipiscing\` elit. \`Maecenas\` tristique justo nisl, non commodo tellus consequat ut. Duis quis augue nibh. [Aliquam](https://www.example.com) erat [volutpat](https://www.example.com). Duis nisi neque, efficitur ut ipsum nec, eleifend pulvinar augue. Aliquam lobortis enim leo. Orci varius natoque penatibus et **maximus** mattis.
-
-// ## Skills
-// ---
-// > Pellentesque **pellentesque**, est sit amet feugiat imperdiet, tortor augue mollis lorem, et commodo magna mi eu nisi. Duis a imperdiet orci, at molestie odio. Nulla efficitur tortor quis eros dictum mollis. Curabitur porttitor vulputate turpis tincidunt ornare. Donec fringilla leo vitae malesuada. Maecenas quis **mi** diam. 
-
-// * C# and stuff
-//   * Lorem _ipsum_ dolor sit amet
-//     * consectetur adipiscing
-//       * tristique justo nisl, 
-//       * non commodo tellus 
-//   * Orci varius natoque 
-//     * consequat ut. Duis 
-//     * quis augue nibh. [Aliquam](https://www.example.com) 
-// * Javascript
-// * Python
-
-// ## Education
-// ---
-// 1. **2017-2020**: University of Life
-// 1. **2009-2016**: School of Rock
-// 1. **2003-2009**: Springfield Elementary
-
-// ## Work History
-// ---
-// | Date              | Position           | Company        | Responsibilities |
-// |-------------------|--------------------|----------------|------------------|
-// |Feb 2021 - present | Software Developer | NBS            | Developing       |
-// |Feb 2018 - Feb 2021| Junior Developer   | Gilmond        | Learning         |
-// |Jun 2006 - Sep 2020| Teacher            | Star Education | Teaching         |
-
-// ## Portfolio
-// ---
-// \`\`\`
-// {
-//   "firstName": "Joe",
-//   "lastName": "Bloggs",
-//   "age": 25
-// }
-// \`\`\`
-// `;
-
 markdown.value = `
+---
+__Advertisement :)__
+
+- __[pica](https://nodeca.github.io/pica/demo/)__ - high quality and fast image
+  resize in browser.
+- __[babelfish](https://github.com/nodeca/babelfish/)__ - developer friendly
+  i18n with plurals support and easy syntax.
+
+You will like those projects!
+
+---
+
+# h1 Heading 8-)
+## h2 Heading
+### h3 Heading
+#### h4 Heading
+##### h5 Heading
+###### h6 Heading
+
+
+## Horizontal Rules
+
+___
+
+---
+
+***
+
+
+## Typographic replacements
+
+Enable typographer option to see result.
+
+(c) (C) (r) (R) (tm) (TM) (p) (P) +-
+
+test.. test... test..... test?..... test!....
+
+!!!!!! ???? ,,  -- ---
+
+"Smartypants, double quotes" and 'single quotes'
+
+
+## Emphasis
+
+**This is bold text**
+
+__This is bold text__
+
+*This is italic text*
+
+_This is italic text_
+
+~~Strikethrough~~
+
+
+## Blockquotes
+
+
+> Blockquotes can also be nested...
+>> ...by using additional greater-than signs right next to each other...
+> > > ...or with spaces between arrows.
+
+
+## Lists
+
+Unordered
+
++ Create a list by starting a line with `+`, `-`, or `*`
++ Sub-lists are made by indenting 2 spaces:
+  - Marker character change forces new list start:
+    * Ac tristique libero volutpat at
+    + Facilisis in pretium nisl aliquet
+    - Nulla volutpat aliquam velit
++ Very easy!
+
+Ordered
+
+1. Lorem ipsum dolor sit amet
+2. Consectetur adipiscing elit
+3. Integer molestie lorem at massa
+
+
+1. You can use sequential numbers...
+1. ...or keep all the numbers as `1.`
+
+Start numbering with offset:
+
+57. foo
+1. bar
+
+
+## Code
+
+Inline `code`
+
+Indented code
+
+    // Some comments
+    line 1 of code
+    line 2 of code
+    line 3 of code
+
+
+Block code "fences"
+
+```
+Sample text here...
+```
+
+Syntax highlighting
+
+``` js
+var foo = function (bar) {
+  return bar++;
+};
+
+console.log(foo(5));
+```
+
+## Tables
+
+| Option | Description |
+| ------ | ----------- |
+| data   | path to data files to supply the data that will be passed into templates. |
+| engine | engine to be used for processing templates. Handlebars is the default. |
+| ext    | extension to be used for dest files. |
+
+Right aligned columns
+
+| Option | Description |
+| ------:| -----------:|
+| data   | path to data files to supply the data that will be passed into templates. |
+| engine | engine to be used for processing templates. Handlebars is the default. |
+| ext    | extension to be used for dest files. |
+
+
+## Links
+
+[link text](http://dev.nodeca.com)
+
+[link with title](http://nodeca.github.io/pica/demo/ "title text!")
+
+Autoconverted link https://github.com/nodeca/pica (enable linkify to see)
+
+
+## Images
+
+![Minion](https://octodex.github.com/images/minion.png)
+![Stormtroopocat](https://octodex.github.com/images/stormtroopocat.jpg "The Stormtroopocat")
+
+Like links, Images also have a footnote style syntax
+
+![Alt text][id]
+
+With a reference later in the document defining the URL location:
+
+[id]: https://octodex.github.com/images/dojocat.jpg  "The Dojocat"
+
+
+## Plugins
+
+The killer feature of `markdown-it` is very effective support of
+[syntax plugins](https://www.npmjs.org/browse/keyword/markdown-it-plugin).
+
+
+### [Emojies](https://github.com/markdown-it/markdown-it-emoji)
+
+> Classic markup: :wink: :crush: :cry: :tear: :laughing: :yum:
+>
+> Shortcuts (emoticons): :-) :-( 8-) ;)
+
+see [how to change output](https://github.com/markdown-it/markdown-it-emoji#change-output) with twemoji.
+
+
+### [Subscript](https://github.com/markdown-it/markdown-it-sub) / [Superscript](https://github.com/markdown-it/markdown-it-sup)
+
+- 19^th^
+- H~2~O
+
+
+### [\<ins>](https://github.com/markdown-it/markdown-it-ins)
+
+++Inserted text++
+
+
+### [\<mark>](https://github.com/markdown-it/markdown-it-mark)
+
+==Marked text==
+
+
 ### [Footnotes](https://github.com/markdown-it/markdown-it-footnote)
 
 Footnote 1 link[^first].
@@ -388,6 +528,47 @@ Duplicated footnote reference[^second].
     and multiple paragraphs.
 
 [^second]: Footnote text.
+
+
+### [Definition lists](https://github.com/markdown-it/markdown-it-deflist)
+
+Term 1
+
+:   Definition 1
+with lazy continuation.
+
+Term 2 with *inline markup*
+
+:   Definition 2
+
+        { some code, part of Definition 2 }
+
+    Third paragraph of definition 2.
+
+_Compact style:_
+
+Term 1
+  ~ Definition 1
+
+Term 2
+  ~ Definition 2a
+  ~ Definition 2b
+
+
+### [Abbreviations](https://github.com/markdown-it/markdown-it-abbr)
+
+This is HTML abbreviation example.
+
+It converts "HTML", but keep intact partial entries like "xxxHTMLyyy" and so on.
+
+*[HTML]: Hyper Text Markup Language
+
+### [Custom containers](https://github.com/markdown-it/markdown-it-container)
+
+::: warning
+*here be dragons*
+:::
+
 `;
 
 run();
